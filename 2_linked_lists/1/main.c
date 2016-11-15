e4377bd1ec6c4fba9eb5ebdb90d93638ab042367
Grayson Pike <graysonpike@gmail.com>
2016-11-15 11:31:26 -0600

    Finished exercise 2.1

--- 2_linked_lists/1/main.c
+++ 2_linked_lists/1/main.c
@@ -4,14 +4,49 @@
 #include <stdlib.h>
 #include "../single.h"
 
+void remove_duplicates(struct node *head) {
+	struct node *i = head;
+	// Traverse list
+	while(i != NULL) {
+		struct node *j = head;
+		struct node *prev = j;
+		// Traverse list for duplicates of this node
+		while(j != NULL) {
+			if(j->value == i->value && j != i) {
+				// Remove this duplicate
+				prev->next = j->next;
+				free(j);
+				j = prev->next;
+			} else {
+				prev = j;
+				j = j->next;
+			}
+		}
+		i = i->next;
+	}
+}
+
 int main() {
 
 	struct node *head = malloc(sizeof(struct node));
 	head->value = 1;
 	head->next = NULL;
+	add_item(head, 1);
 	add_item(head, 2);
 	add_item(head, 3);
+	add_item(head, 4);
+	add_item(head, 3);
+	add_item(head, 5);
+	add_item(head, 6);
+	//add_item(head, 6);
 
+	printf("Before: \n");
+	print_list(head);
+	printf("\n");
+
+	remove_duplicates(head);
+
+	printf("After:  \n");
 	print_list(head);
 	printf("\n");
 
