c20e3c80c28a08bc91df9479ce1c1d8dd92b49a0
Grayson112233 <graysonpike@gmail.com>
2016-12-24 21:38:36 -0600

    Finished 3.2

--- 3_stacks_and_queues/2/main.c
+++ 3_stacks_and_queues/2/main.c
@@ -12,17 +12,43 @@ struct node {
 	struct node *next;
 };
 
+// Creates first node in this stack
+struct node *init_stack(int value) {
+	struct node *head = malloc(sizeof(head));
+	head->value = value;
+	head->min = value;
+	head->next = NULL;
+	return head;
+}
+
 void push(struct node **head, int value) {
 	struct node *new = malloc(sizeof(struct node));
 	new->value = value;
-	new->min = value < min ? value : min;
+	new->min = value < (*head)->min ? value : (*head)->min;
 	new->next = *head;
 	*head = new;
 }
 
+int get_min(struct node *head) {
+	return head->min;
+}
+
+int pop(struct node **head) {
+	struct node *new_head = (*head)->next;
+	int value = (*head)->value;
+	free(*head);
+	*head = new_head;
+	return value;
+}
+
 int main() {
 
-	struct node *head = malloc(sizeof(head));
-	// WIP
+
+	struct node *head = init_stack(3);
+	push(&head, 2);
+	push(&head, 4);
+	printf("Minimum is %d\n", get_min(head));
+	printf("Popped %d and %d\n", pop(&head), pop(&head));
+	printf("Minimum is %d\n", get_min(head));
 
 }
